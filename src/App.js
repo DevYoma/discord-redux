4f2b0f72646f00d07b1b20bbe9a39d6b1e1c5f29
DevYoma <Lawrenceyoma@gmail.com>
2021-06-21 23:13:50 +0100

    first Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <p>This is a Discord Clone 🚀🚀🚀🚀🚀</p>
+      <h1></h1>
+    </div>
+  );
+}
+
+export default App;
